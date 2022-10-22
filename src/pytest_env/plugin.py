5b787854e081037f1c7a604e23a27f9050027638
Elijas <master.elijas@gmail.com>
2022-10-23 00:44:17 +0300

    Add parametrized tests

--- src/pytest_env/plugin.py
+++ src/pytest_env/plugin.py
@@ -2,14 +2,8 @@
 from __future__ import annotations
 
 import os
-from typing import Final
 
 import pytest
-
-_DEFAULT_FLAG: Final = 'D'
-_RAW_FLAG: Final = 'R'
-
-_ALLOWED_FLAGS: Final = (_DEFAULT_FLAG, _RAW_FLAG)
 
 
 def pytest_addoption(parser: pytest.Parser) -> None:
@@ -21,41 +15,16 @@ def pytest_addoption(parser: pytest.Parser) -> None:
 
 @pytest.hookimpl(tryfirst=True)  # type: ignore # untyped decorator
 def pytest_load_initial_conftests(
-        args: list[str], early_config: pytest.Config, parser: pytest.Parser  # noqa: U100
+    args: list[str], early_config: pytest.Config, parser: pytest.Parser  # noqa: U100
 ) -> None:
     """Load environment variables from configuration files."""
-    for e in early_config.getini("env"):
-        part = e.partition("=")
-        # INI key consists of flags and of the env variable key
-        # For example D:R:NAME=VAL has two flags (R and D), NAME key, and VAL value
-        ini_key = part[0].strip()
+    for line in early_config.getini("env"):
+        part = line.partition("=")
+        key = part[0].strip()
         value = part[2].strip()
 
-        ini_key_parts = ini_key.split(":")
-        flags = ini_key_parts[:-1]
-        for flag in flags:
-            if flag not in _ALLOWED_FLAGS:
-                raise Exception(
-                    'Flag "%s" is not recognized. '
-                    'Colons in variable names can only be used to denote flags.' % flag
-                )
-        key = ini_key_parts[-1]
-
-        # use R: as a way to designate whether to use
-        # "raw" value (skip replacing environment
-        # variables in a value). Use this to allow
-        # curly bracket characters in a value.
-        rkey = key.split("R:")
-        use_raw_value = False
-
-        if len(rkey) == 2:
-            key = rkey[1]
-            use_raw_value = True
-
-        # Replace environment variables in value. for instance:
-        # TEST_DIR={USER}/repo_test_dir.
-        if not use_raw_value:
-            value = value.format(**os.environ)
+        # Replace environment variables in value. for instance  TEST_DIR={USER}/repo_test_dir.
+        value = value.format(**os.environ)
 
         # use D: as a way to designate a default value that will only override env variables if they do not exist
         default_key = key.split("D:")
--- tests/test_env.py
+++ tests/test_env.py
@@ -1,90 +1,20 @@
 from __future__ import annotations
 
-from collections import namedtuple
-from typing import Dict
+from pathlib import Path
 
 import pytest
 
-fields = "test_name,existing_env_vars,ini_contents,expected_env_vars"
-TestCase = namedtuple("TestCase", fields)
-all_test_cases = [
-    TestCase(
-        test_name="test_adds_environment_variable",  # name of the test case
-        existing_env_vars={},  # empty dict means no preexisting environment variables
-        ini_contents="[pytest]\nenv = MAGIC=alpha",  # pytest.ini contents
-        expected_env_vars={"MAGIC": "alpha"}
-    ),
-    TestCase(
-        test_name="test_given_existing_env_var_and_returns_new_value",
-        existing_env_vars={"MAGIC": "alpha"},
-        ini_contents="[pytest]\nenv = MAGIC=beta",
-        expected_env_vars={"MAGIC": "beta"}
-    ),
-    TestCase(
-        test_name="test_given_existing_env_var_and_with_default_flag_returns_existing_value",
-        existing_env_vars={"MAGIC": "alpha"},
-        ini_contents="[pytest]\nenv = D:MAGIC=beta",
-        expected_env_vars={"MAGIC": "alpha"}
-    ),
-    TestCase(
-        test_name="test_given_curly_braces_and_no_raw_flag_returns_concatenated",
-        existing_env_vars={"PLANET": "world"},
-        ini_contents="[pytest]\nenv = MAGIC=hello_{PLANET}",
-        expected_env_vars={"MAGIC": "hello_world"}
-    ),
-    TestCase(
-        test_name="test_given_curly_braces_and_raw_flag_returns_raw_value",
-        existing_env_vars={"PLANET": "world"},
-        ini_contents="[pytest]\nenv = R:MAGIC=hello_{PLANET}",
-        expected_env_vars={"MAGIC": "hello_{PLANET}"}
-    ),
-    TestCase(
-        test_name="test_given_curly_braces_take_only_the_newest_value",
-        existing_env_vars={"PLANET": "earth"},
-        ini_contents="[pytest]\nenv = \n    PLANET=mars\n    MAGIC={PLANET}",
-        expected_env_vars={"MAGIC": "mars"}
-    ),
-    TestCase(
-        test_name="test_given_curly_braces_take_only_the_applied_value",
-        existing_env_vars={"PLANET": "earth"},
-        ini_contents="[pytest]\nenv = \n    D:PLANET=mars\n    MAGIC={PLANET}",
-        expected_env_vars={"MAGIC": "earth"}
-    ),
-    TestCase(
-        test_name="test_given_two_flags_applies_both_of_them",
-        existing_env_vars={"MAGIC": "alpha"},
-        ini_contents="[pytest]\nenv = D:R:MAGIC=beta",
-        expected_env_vars={"MAGIC": "alpha"}
-    ),
-    TestCase(
-        test_name="test_given_two_flags_in_reversed_order_applies_both_of_them",
-        existing_env_vars={"MAGIC": "alpha"},
-        ini_contents="[pytest]\nenv = R:D:MAGIC=beta",
-        expected_env_vars={"MAGIC": "alpha"}
-    ),
-]
+
+@pytest.fixture()
+def example(testdir: pytest.Testdir) -> pytest.Testdir:
+    src = Path(__file__).parent / "example.py"
+    dest = Path(str(testdir.tmpdir / "test_example.py"))
+    dest.symlink_to(src)
+    return testdir
 
 
-@pytest.mark.parametrize(fields, all_test_cases)
-def test_cases(testdir: pytest.Testdir, test_name, existing_env_vars, ini_contents, expected_env_vars) -> None:
-    for env_var, val in existing_env_vars.items():
-        testdir.monkeypatch.setenv(env_var, val)
-    create_test(testdir, test_name, expected_env_vars, ini_contents)
-
-    result = testdir.runpytest()
+def test_simple(example: pytest.Testdir) -> None:
+    (example.tmpdir / "pytest.ini").write_text("[pytest]\nenv = MAGIC=alpha", encoding="utf-8")
+    example.monkeypatch.setenv("_PATCH", "alpha")
+    result = example.runpytest()
     result.assert_outcomes(passed=1)
-
-
-def create_test(testdir: pytest.Testdir, test_name: str, expected_env_vars: Dict[str, str], ini_contents: str) -> None:
-    contents = """
-from __future__ import annotations
-import ast
-import os
-
-def %s() -> None:
-    for key, expected_val in ast.literal_eval(\"\"\"%s\"\"\").items():
-        assert os.environ[key] == expected_val
-    """ % (test_name, expected_env_vars)
-
-    (testdir.tmpdir / "test_returns_value.py").write_text(contents, encoding="utf-8")
-    (testdir.tmpdir / "pytest.ini").write_text(ini_contents, encoding="utf-8")
