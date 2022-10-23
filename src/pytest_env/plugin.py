3352f7553410b3d612a7aafa7044a3ad4b9a87ba
Elijas <master.elijas@gmail.com>
2022-10-23 15:11:57 +0300

    Specify Final constant types (#9)

--- src/pytest_env/plugin.py
+++ src/pytest_env/plugin.py
@@ -2,14 +2,14 @@
 from __future__ import annotations
 
 import os
-from typing import Final
+from typing import Final, Tuple
 
 import pytest
 
-_DEFAULT_FLAG: Final = 'D'
-_RAW_FLAG: Final = 'R'
+_DEFAULT_FLAG: Final[str] = 'D'
+_RAW_FLAG: Final[str] = 'R'
 
-_ALLOWED_FLAGS: Final = (_DEFAULT_FLAG, _RAW_FLAG)
+_ALLOWED_FLAGS: Final[Tuple[str, ...]] = (_DEFAULT_FLAG, _RAW_FLAG)
 
 
 def pytest_addoption(parser: pytest.Parser) -> None:
