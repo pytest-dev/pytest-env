92c90a0244a7bd3d0607994324c1254ddd0c282a
Elijas <master.elijas@gmail.com>
2022-10-23 03:45:55 +0300

    Improve readability (use pytest.param) (#9)

--- tests/test_env.py
+++ tests/test_env.py
@@ -1,92 +1,81 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
 from typing import Dict
 
 import pytest
 
 
-@dataclass
-class TestCase:
-    name: str  # name of the test case
-    existing_env_vars: Dict[str, str]  # preexisting environment variables
-    ini_contents: str  # pytest.ini contents
-    expected_env_vars: Dict[str, str]  # variables that are expected to be found after the plugin has done its job
-
-    # This helps pytest output to show test name for each test
-    def __str__(self):
-        return self.name
-
-
-all_test_cases = [
-    TestCase(
-        name="test_adds_environment_variable",
-        existing_env_vars={},  # empty dict means no preexisting environment variables
-        ini_contents="[pytest]\nenv = MAGIC=alpha",
-        expected_env_vars={"MAGIC": "alpha"}
+@pytest.mark.parametrize("name,existing_env_vars,ini_contents,expected_env_vars", [
+    pytest.param(
+        "test_adds_environment_variable",
+        {},  # empty dict means no preexisting environment variables
+        "[pytest]\nenv = MAGIC=alpha",
+        {"MAGIC": "alpha"}
     ),
-    TestCase(
-        name="test_given_existing_env_var_returns_new_value",
-        existing_env_vars={"MAGIC": "alpha"},
-        ini_contents="[pytest]\nenv = MAGIC=beta",
-        expected_env_vars={"MAGIC": "beta"}
+    pytest.param(
+        "test_given_existing_env_var_returns_new_value",
+        {"MAGIC": "alpha"},
+        "[pytest]\nenv = MAGIC=beta",
+        {"MAGIC": "beta"}
     ),
-    TestCase(
-        name="test_given_existing_env_var_and_with_default_flag_returns_existing_value",
-        existing_env_vars={"MAGIC": "alpha"},
-        ini_contents="[pytest]\nenv = D:MAGIC=beta",
-        expected_env_vars={"MAGIC": "alpha"}
+    pytest.param(
+        "test_given_existing_env_var_and_with_default_flag_returns_existing_value",
+        {"MAGIC": "alpha"},
+        "[pytest]\nenv = D:MAGIC=beta",
+        {"MAGIC": "alpha"}
     ),
-    TestCase(
-        name="test_given_curly_braces_and_no_raw_flag_returns_concatenated",
-        existing_env_vars={"PLANET": "world"},
-        ini_contents="[pytest]\nenv = MAGIC=hello_{PLANET}",
-        expected_env_vars={"MAGIC": "hello_world"}
+    pytest.param(
+        "test_given_curly_braces_and_no_raw_flag_returns_concatenated",
+        {"PLANET": "world"},
+        "[pytest]\nenv = MAGIC=hello_{PLANET}",
+        {"MAGIC": "hello_world"}
     ),
-    TestCase(
-        name="test_given_curly_braces_and_raw_flag_returns_raw_value",
-        existing_env_vars={"PLANET": "world"},
-        ini_contents="[pytest]\nenv = R:MAGIC=hello_{PLANET}",
-        expected_env_vars={"MAGIC": "hello_{PLANET}"}
+    pytest.param(
+        "test_given_curly_braces_and_raw_flag_returns_raw_value",
+        {"PLANET": "world"},
+        "[pytest]\nenv = R:MAGIC=hello_{PLANET}",
+        {"MAGIC": "hello_{PLANET}"}
     ),
-    TestCase(
-        name="test_given_curly_braces_take_only_the_newest_value",
-        existing_env_vars={"PLANET": "earth"},
-        ini_contents="[pytest]\nenv = PLANET=mars\n MAGIC={PLANET}",
-        expected_env_vars={"MAGIC": "mars"}
+    pytest.param(
+        "test_given_curly_braces_take_only_the_newest_value",
+        {"PLANET": "earth"},
+        "[pytest]\nenv = PLANET=mars\n MAGIC={PLANET}",
+        {"MAGIC": "mars"}
     ),
-    TestCase(
-        name="test_given_curly_braces_take_only_the_applied_value",
-        existing_env_vars={"PLANET": "earth"},
-        ini_contents="[pytest]\nenv = D:PLANET=mars\n MAGIC={PLANET}",
-        expected_env_vars={"MAGIC": "earth"}
+    pytest.param(
+        "test_given_curly_braces_take_only_the_applied_value",
+        {"PLANET": "earth"},
+        "[pytest]\nenv = D:PLANET=mars\n MAGIC={PLANET}",
+        {"MAGIC": "earth"}
     ),
-    TestCase(
-        name="test_given_two_flags_applies_both_of_them",
-        existing_env_vars={"MAGIC": "alpha"},
-        ini_contents="[pytest]\nenv = D:R:MAGIC=beta",
-        expected_env_vars={"MAGIC": "alpha"}
+    pytest.param(
+        "test_given_two_flags_applies_both_of_them",
+        {"MAGIC": "alpha"},
+        "[pytest]\nenv = D:R:MAGIC=beta",
+        {"MAGIC": "alpha"}
     ),
-    TestCase(
-        name="test_given_two_flags_in_reversed_order_applies_both_of_them",
-        existing_env_vars={"MAGIC": "alpha"},
-        ini_contents="[pytest]\nenv = R:D:MAGIC=beta",
-        expected_env_vars={"MAGIC": "alpha"}
+    pytest.param(
+        "test_given_two_flags_in_reverse_order_applies_both_of_them",
+        {"MAGIC": "alpha"},
+        "[pytest]\nenv = R:D:MAGIC=beta",
+        {"MAGIC": "alpha"}
     ),
-]
-
-
-@pytest.mark.parametrize("testcase", all_test_cases, ids=str)
-def test_cases(testdir: pytest.Testdir, testcase: TestCase) -> None:
-    for env_var, val in testcase.existing_env_vars.items():
-        testdir.monkeypatch.setenv(env_var, val)
-    create_test(testdir, testcase)
-
+], ids=lambda testcase: testcase[0])
+def test_cases(testdir: pytest.Testdir, name: str, existing_env_vars: Dict[str, str], ini_contents: str,
+               expected_env_vars: Dict[str, str]) -> None:
+    set_env_variables(testdir, expected_env_vars)
+    write_to_files(testdir, name, ini_contents, expected_env_vars)
     result = testdir.runpytest()
     result.assert_outcomes(passed=1)
 
 
-def create_test(testdir: pytest.Testdir, testcase: TestCase) -> None:
+def set_env_variables(testdir: pytest.Testdir, env_vars: Dict[str, str]):
+    for env_var, val in env_vars.items():
+        testdir.monkeypatch.setenv(env_var, val)
+
+
+def write_to_files(testdir: pytest.Testdir, name: str, ini_contents: str,
+                   expected_env_vars: Dict[str, str]) -> None:
     contents = """
 from __future__ import annotations
 import ast
@@ -95,7 +84,7 @@ import os
 def %s() -> None:
     for key, expected_val in ast.literal_eval(\"\"\"%s\"\"\").items():
         assert os.environ[key] == expected_val
-    """ % (testcase.name, testcase.expected_env_vars)
+    """ % (name, expected_env_vars)
 
-    (testdir.tmpdir / ("%s.py" % testcase.name)).write_text(contents, encoding="utf-8")
-    (testdir.tmpdir / "pytest.ini").write_text(testcase.ini_contents, encoding="utf-8")
+    (testdir.tmpdir / ("%s.py" % name)).write_text(contents, encoding="utf-8")
+    (testdir.tmpdir / "pytest.ini").write_text(ini_contents, encoding="utf-8")
