4e778871d266c6555553b9560819219790ca456b
Elijas <master.elijas@gmail.com>
2022-10-23 15:17:48 +0300

    Wrap lines at 120 char (#9)

--- src/pytest_env/plugin.py
+++ src/pytest_env/plugin.py
@@ -26,8 +26,8 @@ def pytest_load_initial_conftests(
     """Load environment variables from configuration files."""
     for line in early_config.getini("env"):
         part = line.partition("=")
-        # INI key consists of flags and of the env variable key
-        # For example D:R:NAME=VAL has two flags (R and D), NAME key, and VAL value
+        # INI key consists of flags and of the env variable key For example D:R:NAME=VAL has two flags (R and D),
+        # NAME key, and VAL value
         ini_key = part[0].strip()
         value = part[2].strip()
 
@@ -42,19 +42,15 @@ def pytest_load_initial_conftests(
                 )
         key = ini_key_parts[-1]
 
-        # use R: as a way to designate whether to use
-        # "raw" value (skip replacing environment
-        # variables in a value). Use this to allow
-        # curly bracket characters in a value.
+        # use R: as a way to designate whether to use "raw" value (skip replacing environment variables in a value).
+        # Use this to allow curly bracket characters in a value.
         use_raw_value = _RAW_FLAG in flags
 
-        # use D: as a way to designate a default value
-        # that will only override env variables if they
-        # do not exist already
+        # use D: as a way to designate a default value that will only override env variables if they do not exist
+        # already
         use_default_value = _DEFAULT_FLAG in flags
 
-        # Replace environment variables in value. for instance:
-        # TEST_DIR={USER}/repo_test_dir.
+        # Replace environment variables in value. for instance: TEST_DIR={USER}/repo_test_dir.
         if not use_raw_value:
             value = value.format(**os.environ)
 
