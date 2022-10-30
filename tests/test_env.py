fc9087c3b5058a24bd863811615f1f13a224d102
Bernát Gábor <gaborjbernat@gmail.com>
2022-10-30 18:33:14 +0000

    Bump tools and deps (#13)

--- tests/test_env.py
+++ tests/test_env.py
@@ -95,9 +95,10 @@ import pytest
 def test_env(
     testdir: pytest.Testdir, env: dict[str, str], ini: str, expected_env: dict[str, str], request: pytest.FixtureRequest
 ) -> None:
+    tmp_dir = Path(str(testdir.tmpdir))
     test_name = re.sub(r"\W|^(?=\d)", "_", request.node.callspec.id).lower()
-    Path(str(testdir.tmpdir / f"test_{test_name}.py")).symlink_to(Path(__file__).parent / "template.py")
-    (testdir.tmpdir / "pytest.ini").write_text(ini, encoding="utf-8")
+    Path(str(tmp_dir / f"test_{test_name}.py")).symlink_to(Path(__file__).parent / "template.py")
+    (tmp_dir / "pytest.ini").write_text(ini, encoding="utf-8")
 
     # monkeypatch persists env variables across parametrized tests, therefore using mock.patch.dict
     with mock.patch.dict(os.environ, {**env, "_TEST_ENV": repr(expected_env)}, clear=True):
