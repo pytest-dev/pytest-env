dd1c1de15ee904d679f1cd6ebb6c39710632f1c9
Bernát Gábor <bgabor8@bloomberg.net>
2023-10-23 18:11:23 -0700

    Add native toml support
    Signed-off-by: Bernát Gábor <bgabor8@bloomberg.net>

--- tests/test_env.py
+++ tests/test_env.py
@@ -92,7 +92,7 @@ import pytest
         ),
     ],
 )
-def test_env(
+def test_env_via_pytest(
     testdir: pytest.Testdir,
     env: dict[str, str],
     ini: str,
@@ -116,3 +116,84 @@ def test_env(
         result = testdir.runpytest()
 
     result.assert_outcomes(passed=1)
+
+
+@pytest.mark.parametrize(
+    ("env", "toml", "ini", "expected_env"),
+    [
+        pytest.param(
+            {},
+            '[tool.pytest.ini_options]\nenv = ["MAGIC=toml", "MAGIC_2=toml2"]',
+            "[pytest]\nenv = MAGIC=ini\n MAGIC_2=ini2",
+            {"MAGIC": "ini", "MAGIC_2": "ini2"},
+            id="ini over toml ini_options",
+        ),
+        pytest.param(
+            {},
+            '[tool.pytest.ini_options]\nenv = ["MAGIC=toml", "MAGIC_2=toml2"]',
+            "",
+            {"MAGIC": "toml", "MAGIC_2": "toml2"},
+            id="toml via ini_options",
+        ),
+        pytest.param(
+            {},
+            '[tool.pytest_env.env]\nMAGIC = 1\nMAGIC_2 = "toml2"',
+            "",
+            {"MAGIC": "1", "MAGIC_2": "toml2"},
+            id="toml native",
+        ),
+        pytest.param(
+            {},
+            '[tool.pytest_env.env]\nMAGIC = 1\nMAGIC_2 = "toml2"',
+            "[pytest]\nenv = MAGIC=ini\n MAGIC_2=ini2",
+            {"MAGIC": "1", "MAGIC_2": "toml2"},
+            id="toml native over ini",
+        ),
+        pytest.param(
+            {},
+            '[tool.pytest_env.env]\nMAGIC = {value = "toml", "transform"= true, "skip_if_set" = true}',
+            "",
+            {"MAGIC": "toml"},
+            id="toml inline table",
+        ),
+    ],
+)
+def test_env_via_toml(  # noqa: PLR0913
+    testdir: pytest.Testdir,
+    env: dict[str, str],
+    toml: str,
+    ini: str,
+    expected_env: dict[str, str],
+    request: pytest.FixtureRequest,
+) -> None:
+    tmp_dir = Path(str(testdir.tmpdir))
+    test_name = re.sub(r"\W|^(?=\d)", "_", request.node.callspec.id).lower()
+    Path(str(tmp_dir / f"test_{test_name}.py")).symlink_to(Path(__file__).parent / "template.py")
+    if ini:
+        (tmp_dir / "pytest.ini").write_text(ini, encoding="utf-8")
+    (tmp_dir / "pyproject.toml").write_text(toml, encoding="utf-8")
+
+    new_env = {
+        **env,
+        "_TEST_ENV": repr(expected_env),
+        "PYTEST_DISABLE_PLUGIN_AUTOLOAD": "1",
+        "PYTEST_PLUGINS": "pytest_env.plugin",
+    }
+
+    # monkeypatch persists env variables across parametrized tests, therefore using mock.patch.dict
+    with mock.patch.dict(os.environ, new_env, clear=True):
+        result = testdir.runpytest()
+
+    result.assert_outcomes(passed=1)
+
+
+def test_env_via_toml_bad(testdir: pytest.Testdir) -> None:
+    toml_file = Path(str(testdir.tmpdir)) / "pyproject.toml"
+    toml_file.write_text("bad toml", encoding="utf-8")
+
+    result = testdir.runpytest()
+    assert result.ret == 4
+    assert result.errlines == [
+        f"ERROR: {toml_file}: Expected '=' after a key in a key/value pair (at line 1, column 5)",
+        "",
+    ]
